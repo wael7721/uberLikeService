8e7ce6ec8e257527507138fb1773c0adc2ab2891
Wael Farhat <Wael7721@outlook.com>
2025-05-02 16:20:31 +0100

    Database connection ready

--- db/init.sql
+++ db/init.sql
@@ -1,47 +1,58 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TYPE ROLE AS ENUM ('passenger', 'taxi', 'admin');
+
+CREATE TABLE IF NOT EXISTS users(
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
   email VARCHAR(100) UNIQUE NOT NULL,
   password Text NOT NULL,
-  user_role ENUM('passenger', 'driver', 'admin') NOT NULL,
+  user_role ROLE NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
 
+
+CREATE TYPE AVAILABILITY as ENUM('available','unavailable');
+
 CREATE TABLE IF NOT EXISTS vehicles(
   id SERIAL PRIMARY KEY,
-  driver_id INTEGER REFERENCES users(id)
-  plate_number VARCHAR,
-  model VARCHAR,
+  driver_id INTEGER REFERENCES users(id) NOT NULL,
+  plate_number VARCHAR NOT NULL,
+  model VARCHAR NOT NULL,
   color VARCHAR,
-  status ENUM('available','unavailable')
+  vehicle_status AVAILABILITY DEFAULT 'available'
 );
+
+
+CREATE TYPE STATUS as ENUM('requested', 'accepted', 'in_progress', 'completed', 'cancelled');
 
 CREATE TABLE IF NOT EXISTS trips(
 id SERIAL PRIMARY KEY,
-driver_id INTEGER REFERENCES users(id),
-passenger_id INTEGER REFERENCES users(id),
-pickup_location VARCHAR,
-dropoff_location VARCHAR,
-status ENUM('requested', 'accepted', 'in_progress', 'completed', 'cancelled'),
-fare DECIMAL,
+driver_id INTEGER REFERENCES users(id) NOT NULL,
+passenger_id INTEGER REFERENCES users(id) NOT NULL,
+pickup_location VARCHAR NOT NULL,
+dropoff_location VARCHAR NOT NULL,
+trip_status STATUS DEFAULT 'requested',
+fare DECIMAL(5,2),
 start_time TIMESTAMP,
 end_time TIMESTAMP
 );
 
+
+CREATE TYPE PAYMENTMETHOD as ENUM('cash','card');
+
 CREATE TABLE IF NOT EXISTS payments (
 id SERIAL PRIMARY KEY,
-trips_id INTEGER REFERENCES trips(id)
-method ENUM('cash','card')
-paid BOOLEAN DEFAULT false
-created_at TIMESTAMP
+trip_id INTEGER REFERENCES trips(id) NOT NULL,
+payment PAYMENTMETHOD,
+paid BOOLEAN DEFAULT false,
+created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS rating(
 id SERIAL PRIMARY KEY,
-from_user_id INTEGER REFERENCES users(id),
-to_user_id INTEGER REFERENCES users(id),
-trip_id INTEGER REFERENCES trips(id),
+from_user_id INTEGER REFERENCES users(id )NOT NULL,
+to_user_id INTEGER REFERENCES users(id) NOT NULL,
+trip_id INTEGER REFERENCES trips(id) NOT NULL,
 rating INTEGER CHECK (rating>=1 AND rating<=5),
 review Text,
-created_at TIMESTAMP
+created_at TIMESTAMP DEFAULT NOW()
 );
