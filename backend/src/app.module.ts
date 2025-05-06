472263467ae55dee74b1df7c4178af1d6e6b6f87
Wael Farhat <Wael7721@outlook.com>
2025-05-06 14:01:29 +0100

    Linked DB to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true, // makes config available app-wide
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        port: 5432,
+        entities: [],
+        database: configService.get<string>('POSTGRES_DB'),
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
